b80ad96880ddc3f26a17f516de58bd5615937aeb
moodloi <moodloid@gmail.com>
2026-02-27 16:29:43 +0400

    feat: default login

---
+++ app/Filament/Pages/Auth/Login.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Filament\Pages\Auth;
+
+class Login extends \Filament\Auth\Pages\Login
+{
+    public function mount(): void
+    {
+        parent::mount();
+
+        $this->form->fill([
+            'email' => 'demo@demo.com',
+            'password' => 'demo',
+            'remember' => true,
+        ]);
+    }
+}
