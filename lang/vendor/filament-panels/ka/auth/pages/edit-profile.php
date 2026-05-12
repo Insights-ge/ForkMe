bb49311844d342be489ae68964ef279faf9308f3
moodloi <moodloid@gmail.com>
2026-05-12 15:55:59 +0400

    chore: localizations

---
+++ lang/vendor/filament-panels/ka/auth/pages/edit-profile.php
@@ -0,0 +1,51 @@
+<?php
+
+return [
+
+    'label' => 'პროფილი',
+
+    'form' => [
+
+        'email' => [
+            'label' => 'ელ. ფოსტა',
+        ],
+
+        'name' => [
+            'label' => 'სახელი',
+        ],
+
+        'password' => [
+            'label' => 'ახალი პაროლი',
+        ],
+
+        'password_confirmation' => [
+            'label' => 'დაადასტურეთ ახალი პაროლი',
+        ],
+
+        'actions' => [
+
+            'save' => [
+                'label' => 'ცვლილებების შენახვა',
+            ],
+
+        ],
+
+    ],
+
+    'notifications' => [
+
+        'saved' => [
+            'title' => 'შენახულია',
+        ],
+
+    ],
+
+    'actions' => [
+
+        'cancel' => [
+            'label' => 'გაუქმება',
+        ],
+
+    ],
+
+];
