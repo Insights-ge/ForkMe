2bb37b9b6cf5544ba47e7263c4e45cc1d6614921
moodloi <moodloid@gmail.com>
2026-05-12 17:09:02 +0400

    feat: add Radar and Polar Area charts, clean up unused assets, and add missing FilePond locale

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,7 +21,7 @@
         href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&family=Inter:wght@400;500;600&display=swap"
         rel="stylesheet">
 
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/css/app.css'])
 
     @stack('head')
 </head>
